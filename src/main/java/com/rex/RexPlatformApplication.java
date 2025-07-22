aa7819d593d1c2979d008af5a92b7d08c7eb2085
raunak-choudhary <raunakchoudhary17@gmail.com>
2025-07-22 15:40:00 -0400

    feat: initial Spring Boot project setup with dependencies

---
+++ src/main/java/com/rex/RexPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.rex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RexPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RexPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rex/RexPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RexPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
